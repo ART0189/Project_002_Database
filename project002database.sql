602ffc4d8b3c89eee810339f13251cf50ef80d9a
ART0189 <1264931036@qq.com>
2021-08-30 17:35:46 +0800

    Create project002database.sql

---
+++ project002database.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : TestMySQLServer
+ Source Server Type    : MySQL
+ Source Server Version : 80026
+ Source Host           : localhost:3306
+ Source Schema         : project002database
+
+ Target Server Type    : MySQL
+ Target Server Version : 80026
+ File Encoding         : 65001
+
+ Date: 30/08/2021 17:35:19
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
