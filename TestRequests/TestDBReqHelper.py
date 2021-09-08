d2cc6d44bdd72a03a9947f89287b36aaadf22151
ART0189 <1264931036@qq.com>
2021-09-08 20:11:01 +0800

    Update TestDBReqHelper.py

--- TestRequests/TestDBReqHelper.py
+++ TestRequests/TestDBReqHelper.py
@@ -10,9 +10,9 @@ def RegisterBaseRequest(playername, password, telehone):
     RequestData = {'playername': playername, 'password': password, 'telephone': telehone}
     RequestHandle = requests.post(BaseUrl + AddUrl, data=RequestData, timeout=200)
     if (RequestHandle.status_code == 200):
-        return 1,RequestHandle.content['baseid'],"ErrorMessage","AdditionalErrorMessage"
+        return 1,RequestHandle.content['baseid'],"ErrorMessage"
     else:
-        return 0,-1,"ErrorMessage","AdditionalErrorMessage"
+        return 0,-1,"ErrorMessage"
 
 
 def Register002Request(playername, playerbaseid, hardwarecode):
@@ -20,24 +20,24 @@ def Register002Request(playername, playerbaseid, hardwarecode):
     RequestData = {'playername': playername, 'baseid': playerbaseid, 'hardwarecode': hardwarecode}
     RequestHandle = requests.post(BaseUrl + AddUrl, data=RequestData, timeout=200)
     if (RequestHandle.status_code == 200):
-        return 1,"ErrorMessage","AdditionalErrorMessage"
+        return 1,"ErrorMessage"
     else:
-        return 0,"ErrorMessage","AdditionalErrorMessage"
+        return 0,"ErrorMessage"
 
 def LoginBaseRequest(baseid,playername,password,hardwarecode):
     AddUrl = 'baseinfomodify/login'
     RequestData={'baseid':baseid,'basename':playername,'password':password,'hardwarecode':hardwarecode}
     RequestHandle=requests.post(BaseUrl+AddUrl,data=RequestData,timeout=200)
     if(RequestHandle.status_code==200):
-        return 1,RequestHandle.content['baseid'],"ErrorMessage","AdditionalErrorMessage"
+        return 1,RequestHandle.content['baseid'],"ErrorMessage"
     else:
-        return 0,-1,"ErrorMessage","AdditionalErrorMessage"
+        return 0,-1,"ErrorMessage"
 
 def Login002Request(playername, hardwarecode):
     AddUrl = 'playermodify002/login'
     RequestData = {'playername': playername, 'hardwarecode': hardwarecode}
     RequestHandle = requests.post(BaseUrl + AddUrl, data=RequestData, timeout=200)
     if (RequestHandle.status_code == 200):
-        return 1,"ErrorMessage","AdditionalErrorMessage"
+        return 1,"ErrorMessage"
     else:
-        return 0,"ErrorMessage","AdditionalErrorMessage"
+        return 0,"ErrorMessage"
