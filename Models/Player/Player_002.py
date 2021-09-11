4c7d26f162c17680e9332432c77ffc135eed2571
ART0189 <1264931036@qq.com>
2021-09-11 18:32:05 +0800

    Update Player_002.py

--- Models/Player/Player_002.py
+++ Models/Player/Player_002.py
@@ -8,9 +8,13 @@ class Player_002(db.Model, EntityBase):
     # BaseInfo
     PlayerID = db.Column(db.INTEGER, primary_key=True, autoincrement=True)
     PlayerName = db.Column(db.String(10), nullable=False)
-    HeadPortrait = db.Column(db.String(100), nullable=False, default='NeedInit')
+    HeadPortrait = db.Column(db.INTEGER, nullable=False, default=1)
     PlayerLv = db.Column(db.INTEGER, nullable=False, default=0)
     PlayerExp = db.Column(db.INTEGER, nullable=False, default=0)
+    PlayerRank = db.Column(db.String(10))
+    PlayerToken = db.Column(db.INTEGER,nullable=False,default=0)
+    PlayerToken_1 = db.Column(db.INTEGER,nullable=False,default=0)
+    PlayerDefaultOOBName = db.Column(db.String(10))
     HaveLogin = db.Column(db.Boolean, nullable=False, default=False)
 
     # Check
